3fdad8f8daad5f6786c72ee63fa65d2efe4bbbd0
Nicolás Salas V <nikosalas@gmail.com>
2018-12-23 04:04:56 -0300

    test: create simple author test

---
+++ test/authors.test.js
@@ -0,0 +1,21 @@
+import Author from '../models/Author'
+import { expect } from 'chai'
+
+describe('Author', () => {
+  describe('#create', () => {
+    it('should create ok with name and biography', async function () {
+      let expectedName = 'jperez'
+      let expectedBio = 'just another brick in the wall'
+      let withNameAndBio = {
+        'name': expectedName,
+        'biography': expectedBio
+      }
+      let created = await Author.create(withNameAndBio)
+
+      expect(created.name).to.be.a('string')
+      expect(created.name).to.equal(expectedName)
+      expect(created.biography).to.be.a('string')
+      expect(created.biography).to.equal(expectedBio)
+    })
+  })
+})
