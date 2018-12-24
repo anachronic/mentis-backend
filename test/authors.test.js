250fedfadd7aea5fcc43ef48626ccce894208fcd
Nicolás Salas V <nikosalas@gmail.com>
2018-12-24 05:20:35 -0300

    test: reorganize structure for author tests

--- test/authors.test.js
+++ test/authors.test.js
@@ -19,43 +19,42 @@ describe('Author', function () {
       expect(created.biography).to.be.a('string')
       expect(created.biography).to.equal(expectedBio)
     })
+  })
 
-    describe('#delete', function () {
-      it('should erase all author\'s posts when deleting the author', async function () {
-        let author = await Author.create({
-          'name': 'yei perez',
-          'biography': 'with another biography'
-        })
-
-        let postOne = await Post.create({
-          title: 'title for post one',
-          content: 'the content for post one',
-          author: author._id
-        })
-
-        let postTwo = await Post.create({
-          title: 'title for post two',
-          content: 'the content for post two',
-          author: author._id
-        })
-
-        let authorId = author._id
-        let authorName = author.name
-        let postIds = [postOne._id, postTwo._id]
-
-        await Author.deleteByName(authorName)
-
-        let foundAuthor = await Author.findById(authorId).exec()
-        console.log(foundAuthor)
-        expect(foundAuthor).to.be.null
-
-        let foundPosts = await Post.find({
-          _id: {
-            $in: postIds
-          }
-        }).exec()
-        expect(foundPosts).to.be.empty
+  describe('#delete', function () {
+    it('should erase all author\'s posts when deleting the author', async function () {
+      let author = await Author.create({
+        'name': 'yei perez',
+        'biography': 'with another biography'
       })
+
+      let postOne = await Post.create({
+        title: 'title for post one',
+        content: 'the content for post one',
+        author: author._id
+      })
+
+      let postTwo = await Post.create({
+        title: 'title for post two',
+        content: 'the content for post two',
+        author: author._id
+      })
+
+      let authorId = author._id
+      let authorName = author.name
+      let postIds = [postOne._id, postTwo._id]
+
+      await Author.deleteByName(authorName)
+
+      let foundAuthor = await Author.findById(authorId).exec()
+      expect(foundAuthor).to.be.null
+
+      let foundPosts = await Post.find({
+        _id: {
+          $in: postIds
+        }
+      }).exec()
+      expect(foundPosts).to.be.empty
     })
   })
 })
