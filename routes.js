b0e91ef65346531d0dc5d1db84bf0f577cbec9be
Nicolás Salas V <nikosalas@gmail.com>
2018-12-24 14:29:16 -0300

    feat: validate authors on creating

--- routes.js
+++ routes.js
@@ -1,10 +1,13 @@
 const PostsController = require('./controllers/PostsController')
 const AuthorController = require('./controllers/AuthorController')
 const asyncHelper = require('./middleware/AsyncHelper')
+const AuthorValidator = require('./validators/AuthorValidator')
 
 module.exports = (express) => {
   express.get('/posts', asyncHelper(PostsController.getAll))
   express.post('/posts', asyncHelper(PostsController.create))
 
-  express.post('/author', asyncHelper(AuthorController.create))
+  express.post('/author',
+    asyncHelper(AuthorValidator.validate),
+    asyncHelper(AuthorController.create))
 }
---
+++ validators/AuthorValidator.js
@@ -0,0 +1,21 @@
+const joi = require('joi')
+
+module.exports = {
+  async validate (req, res, next) {
+    const schema = joi.object().keys({
+      name: joi.string().required().trim(),
+      biography: joi.string()
+    })
+
+    let validationResult = joi.validate(req.body, schema)
+
+    if (validationResult.error) {
+      res.status(400).send({
+        status: 'error',
+        message: 'Data provided was not valid'
+      })
+    } else {
+      next()
+    }
+  }
+}
