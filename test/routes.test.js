cb0d839f9f01cb691b1529e9e79981557700a1b9
Nicolás Salas V <nikosalas@gmail.com>
2018-12-24 13:50:14 -0300

    test(routes): POSTing new authors

--- test/routes.test.js
+++ test/routes.test.js
@@ -11,11 +11,61 @@ describe('Route testing', function () {
       chai.request(server)
         .get('/posts')
         .end((err, res) => {
-          if (err) {
-            expect.fail()
-          }
+          expect(err).to.be.null
           expect(res).to.be.json
+          expect(res).to.have.status(200)
 
+          done()
+        })
+    })
+  })
+
+  describe('POST#authors', function () {
+    it('will not return 404 not found', function (done) {
+      chai.request(server).post('/author').end(function (err, res) {
+        expect(err).to.be.null
+        expect(res).to.not.have.status(404)
+        done()
+      })
+    })
+
+    it('will succeed with a good-formed author object', function (done) {
+      chai.request(server)
+        .post('/author')
+        .send({
+          name: "the author's name",
+          biography: 'a cool biography about this author'
+        })
+        .end(function (err, res) {
+          expect(err).to.be.null
+          expect(res).to.be.json
+          expect(res).to.have.status(200)
+          done()
+        })
+    })
+
+    it('will suceed with good author object with no biography', function (done) {
+      chai.request(server)
+        .post('/author')
+        .send({
+          name: 'another author'
+        })
+        .end(function (err, res) {
+          expect(err).to.be.null
+          expect(res).to.be.json
+          expect(res).to.have.status(200)
+          done()
+        })
+    })
+
+    it('will not succeed with an empty object', function (done) {
+      chai.request(server)
+        .post('/author')
+        .send({})
+        .end(function (err, res) {
+          expect(err).to.be.null
+          expect(res).to.be.json
+          expect(res).to.have.status(400)
           done()
         })
     })
