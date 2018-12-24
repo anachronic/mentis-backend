b0e994e12c6878c8fc1941ac761dd52b794c24dd
Nicolás Salas V <nikosalas@gmail.com>
2018-12-23 22:04:23 -0300

    test: add tests for removal of authors

--- test/authors.test.js
+++ test/authors.test.js
@@ -1,11 +1,13 @@
 import Author from '../models/Author'
+import Post from '../models/Post'
 import { expect } from 'chai'
+
+var expectedName = 'jperez'
+var expectedBio = 'just another brick in the wall'
 
 describe('Author', function () {
   describe('#create', function () {
     it('should create ok with name and biography', async function () {
-      let expectedName = 'jperez'
-      let expectedBio = 'just another brick in the wall'
       let withNameAndBio = {
         'name': expectedName,
         'biography': expectedBio
@@ -16,6 +18,40 @@ describe('Author', function () {
       expect(created.name).to.equal(expectedName)
       expect(created.biography).to.be.a('string')
       expect(created.biography).to.equal(expectedBio)
+    })
+
+    describe('#delete', function () {
+      it('should erase all author\'s posts when deleting the author', async function () {
+        let author = await Author.create({
+          'name': expectedName,
+          'biography': expectedBio
+        })
+
+        let postOne = await Post.create({
+          title: 'title for post one',
+          content: 'the content for post one'
+        })
+
+        let postTwo = await Post.create({
+          title: 'title for post one',
+          content: 'the content for post one'
+        })
+
+        let authorId = author._id
+        let postIds = [postOne._id, postTwo._id]
+
+        await Author.remove({ _id: authorId })
+
+        let foundAuthor = await Author.findOne({ _id: authorId })
+        expect(foundAuthor).to.be.null
+
+        let foundPosts = await Post.find({
+          _id: {
+            $in: postIds
+          }
+        })
+        expect(foundPosts).to.be.empty
+      })
     })
   })
 })
