cb1d4c5bc6c92e275ebe49b6678db19bf75d3034
Nicolás Salas V <nikosalas@gmail.com>
2018-12-26 00:16:03 -0300

    test(post routes): create tests for getting a single post

--- test/routes/posts.test.js
+++ test/routes/posts.test.js
@@ -2,11 +2,12 @@ const chai = require('chai')
 const expect = chai.expect
 const server = require('../../server')
 const chaiHttp = require('chai-http')
+const { Post, Author } = require('../../models')
 
 chai.use(chaiHttp)
 
 describe("Post's routes", function () {
-  describe('GET#posts', function () {
+  describe('GET all posts', function () {
     it('should return ok with no args', function (done) {
       chai.request(server)
         .get('/posts')
@@ -20,7 +21,20 @@ describe("Post's routes", function () {
     })
   })
 
-  describe('GET#post', function () {
+  var createdPost
+
+  describe('GET a post', function () {
+    before(async function () {
+      let author = await Author.create({ name: 'routes author' })
+      let post = await Post.create({
+        title: 'post for routes',
+        content: 'some content',
+        author: author._id
+      })
+
+      createdPost = post
+    })
+
     it("will return 404 with an argument that isn't a post", function (done) {
       chai.request(server)
         .get('/posts/82349jmalsx')
@@ -33,7 +47,16 @@ describe("Post's routes", function () {
         })
     })
 
-    // it('will return the post if it previously exists', function (done) {
-    // })
+    it('will return the post if it previously exists', function (done) {
+      chai.request(server)
+        .get(`/posts/${createdPost._id}`)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.be.json
+          expect(res).to.have.status(200)
+
+          done()
+        })
+    })
   })
 })
